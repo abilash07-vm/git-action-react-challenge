74716dc708ffdf9d79001509632b341865e66298
abilash07-vm <abilashvm07@gmail.com>
2025-07-14 23:29:58 +0530

    Added inputs and outputs

--- .github/actions/simple-node-action/index.js
+++ .github/actions/simple-node-action/index.js
@@ -7,6 +7,9 @@ var exec = require('@actions/exec');
 function getSumOfTwoNum(x, y){
     return x + y;
 }
-
-var sumVal = getSumOfTwoNum(1, 2);
+var num1 = core.getInput('num1',{required: true});
+var num2 = core.getInput('num2',{required: true});
+var sumVal = getSumOfTwoNum(num1, num2);
 core.notice(`Sum value is ${sumVal}`);
+
+core.setOutput('sumVal', sumVal);
